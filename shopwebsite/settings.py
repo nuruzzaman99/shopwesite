44a1c9a6b8c78961ddb0efa714f4b064c296d27a
Md. Nuruzzaman <46056987+nuruzzaman99@users.noreply.github.com>
2022-08-25 18:00:03 +0600

    Update settings.py

--- shopwebsite/settings.py
+++ shopwebsite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nd-er=^mwikjlx68xsn_l(wj8s@-8qhu6fuvb$__kzj%2=@z_y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
